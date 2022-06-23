3a1b32b9327fe9d29cd351a767e104c9678a7127
Steven McKeown <stevenmckeown19@gmail.com>
2022-06-23 15:09:38 +0100

    Added function to return coordinates consistent across devices

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,9 +2,21 @@ import boardImg from "../assets/boards/ps4.jpg";
 import "./Game.css";
 
 const Game = () => {
+    const onClick = (e) => {
+        const xCoord = Math.round(
+            (e.nativeEvent.offsetX / e.nativeEvent.target.offsetWidth) * 100
+        );
+        const yCoord = Math.round(
+            (e.nativeEvent.offsetY / e.target.offsetHeight) * 100
+        );
+        const coords = { xCoord, yCoord };
+        console.log(coords);
+        return coords;
+    };
+
     return (
         <div className="container">
-            <img src={boardImg} alt="ps4" />
+            <img src={boardImg} alt="ps4" className="board" onClick={onClick} />
         </div>
     );
 };
